d31de12a98d5ab90e98fd15ddf6ec14bbd3adb29
Alexandr-Mikhailov <mikhailov_as@mail.ru>
2020-03-01 20:13:51 +0400

    feat: hanoi tower implemented

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,5 +1,3 @@
-module.exports = function repeater(/* str, options */) {
-    throw 'Not implemented';
-    // remove line with error and write your code here
+module.exports = function repeater(str, options) {
 };
   
