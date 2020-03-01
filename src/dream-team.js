12ca4f8ed0785ca6f8797f2179f771d3c6b3a1ab
Alexandr-Mikhailov <mikhailov_as@mail.ru>
2020-03-01 14:43:12 +0400

    feat: dream team implemented

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,4 +1,17 @@
-module.exports = function createDreamTeam(/* members */) {
-  throw 'Not implemented';
-  // remove line with error and write your code here
+module.exports = function createDreamTeam(members) {
+  if(!Array.isArray(members)) {
+    return false;
+  }
+  if (!members.length) {
+    return '';
+  }
+
+  let team = [];
+  
+  for (let i = 0; i < members.length; i++) {
+    if (typeof(members[i]) == 'string') {
+      team.push(members[i].trim().charAt(0).toUpperCase());
+    }
+  }
+  return team.sort().join('');
 };
