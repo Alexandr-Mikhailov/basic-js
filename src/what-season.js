fb8039b88c01a90f03598971a4c9b8ad6dc5c261
Alexandr-Mikhailov <mikhailov_as@mail.ru>
2020-10-06 19:53:26 +0400

    feat: add what-season task implementation

--- src/what-season.js
+++ src/what-season.js
@@ -1,4 +1,27 @@
-module.exports = function getSeason(/* date */) {
-    throw 'Not implemented';
-    // remove line with error and write your code here
+module.exports = function getSeason(date) {
+
+    if (typeof(date) === 'undefined') {
+        return 'Unable to determine the time of year!';
+    }
+
+    var toString = {}.toString;
+
+    if (toString.call(date) !== '[object Date]') {
+        throw new Error('Error');
+    }
+
+    let month = date.getMonth();
+    
+    switch (true) {
+        case month >= 0 && month <=1 || month === 11:
+            return 'winter';
+        case month >= 2 && month <=4:
+            return 'spring';
+        case month >= 5 && month <=7:
+            return 'summer';
+        case month >= 8 && month <=10:
+            return 'autumn';
+        default:
+            break;
+    }
 };
