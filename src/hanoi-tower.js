1a43d7fe8fd8eb7b598330d1e2ce80a40f369bcc
Alexandr-Mikhailov <mikhailov_as@mail.ru>
2020-10-11 13:02:44 +0400

    fix: floor seconds in  hanoi-tower task

--- src/hanoi-tower.js
+++ src/hanoi-tower.js
@@ -1,5 +1,5 @@
 module.exports = function calculateHanoi(disksNumber, turnsSpeed) {
     let turns = Math.pow(2, disksNumber) - 1;
-    let seconds = (Math.pow(2, disksNumber) - 1) / (turnsSpeed / 3600);
+    let seconds =Math.floor((Math.pow(2, disksNumber) - 1) / (turnsSpeed / 3600));
     return {turns, seconds};
 }
